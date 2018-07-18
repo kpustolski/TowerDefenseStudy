820c40c141797ae8c67a3e54d0a22deb78404b1d
Katie Pustolski <pustolskik@gmail.com>
2018-07-18 14:57:44 -0700

    First commit. Got pathfinding and camera movement working.

---
+++ TowerDefenseStudy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private int _totalGold;
+    public int TotalGold{
+        get{
+            return _totalGold;
+        }
+        set{
+            this._totalGold = value;
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void DecreaseGold(int amount){
+        TotalGold -= amount;
+    }
+    public void IncreaseGold(int amount){
+        TotalGold += amount;
+    }
+}
---
+++ TowerDefenseStudy/Assets/Scripts/Unit.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// https://github.com/SebLague/Pathfinding
+/// https://www.youtube.com/watch?v=dn1XRIaROM4&t=232s
+/// </summary>
+public class Unit : MonoBehaviour
+{
+
+
+    public Transform target;
+    public float speed = 20;
+    Vector3[] path;
+    int targetIndex;
+
+    void Start()
+    {
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            targetIndex = 0;
+            StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+        while (true)
+        {
+            if (transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if (targetIndex >= path.Length)
+                {
+                    // Code where unit ends journey
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+
+            yield return null;
+
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(path[i], Vector3.one);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
+    }
+}
